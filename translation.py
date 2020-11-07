c30d9e0dc0c20fc8b95e4ca865fdab6691ba4ad5
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-11-07 16:58:45 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,5 @@
+class Translation(object):
+    START_MSG = """<b>Hello,
+
+I am a Simple Bot Forward The Files You Send to Me to your Channel</b>""" 
+    HELP = """<code>Send me Any Medias to Forward to the Channel</code>"""  
