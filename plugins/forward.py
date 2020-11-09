bfb7a55202a5679962d698c7ed76778e9c016c82
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-11-09 14:54:57 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -52,3 +52,22 @@ async def forward(bot, message):
            parse_mode='html',
            quote=True
        )
+
+@pyrogram.Client.on_message(Filters.group & Filters.media)
+async def forward(bot, message):
+    try:
+       await message.forward(
+           chat_id=Config.CHANNEL_ID,
+           as_copy=True
+       )
+       await message.reply_text(
+           text="<code>Forwaded Sucessfully</code>",
+           parse_mode='html',
+           quote=True
+       )
+    except:
+       await message.reply_text(
+           text="<code>Make Sure That I am Admin in Your Channel or Provided Channel ID is Correct.</code>",
+           parse_mode='html',
+           quote=True
+       )
